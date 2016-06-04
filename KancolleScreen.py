20a7dd7edaeca3504c8c8cdd556f84c2ae6b0ca8
solv <solv301@gmail.com>
2016-06-04 23:11:23 +0900

    初期準備

---
+++ KancolleScreen.py
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from logging import getLogger,StreamHandler,DEBUG
+
+# ログ設定
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
